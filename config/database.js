e88ff014756ccce80aff39114f51e73a695cc292
Cesar Fabri <cesar.fabrii@gmail.com>
2021-01-01 17:42:02 -0300

    Alterado endereço API

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '192.168.0.10'),
+        host: env('DATABASE_HOST', '189.115.46.21'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'dicenter'),
         username: env('DATABASE_USERNAME', 'cesar'),
