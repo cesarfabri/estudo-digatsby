d534e68e075433bc668e0c68085300ba0b1f1ffd
Cesar Fabri <cesar.fabrii@gmail.com>
2021-01-01 17:36:24 -0300

    Primeiro commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '48ccdabf706cbcc83df9eb533582f01b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ed67ae25-5239-403f-97ec-9eec4e5868cd'
+};
